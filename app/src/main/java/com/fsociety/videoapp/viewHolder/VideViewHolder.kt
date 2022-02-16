c4b5fcfb7107c9494d171a1c664c6b9f92ba7906
LenouarMiloud <lenouarmiloud@gmail.com>
2022-02-16 18:24:17 +0100

    add the videoViewHolder in the app

---
+++ app/src/main/java/com/fsociety/videoapp/viewHolder/VideViewHolder.kt
@@ -0,0 +1,19 @@
+package com.fsociety.videoapp.viewHolder
+
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.VideoView
+import androidx.recyclerview.widget.RecyclerView
+import com.fsociety.videoapp.R
+
+class VideViewHolder(view: View):RecyclerView.ViewHolder(view) {
+
+    val commentImage : ImageView = view.findViewById(R.id.commentImage)
+    val likeImage : ImageView = view.findViewById(R.id.likeImage)
+    val shareImage : ImageView = view.findViewById(R.id.shareImage)
+    val likeText : TextView = view.findViewById(R.id.likeNumber)
+    val videoView : VideoView = view.findViewById(R.id.videoView)
+
+
+}
